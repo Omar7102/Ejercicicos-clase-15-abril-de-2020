66d2fadbcdd045f06d76603ffbb9fbb1d645fdc6
Omar7102 <62663061+Omar7102@users.noreply.github.com>
2020-04-16 18:51:55 -0500

    Update LongitudSombraEdificio.py

--- LongitudSombraEdificio.py
+++ LongitudSombraEdificio.py
@@ -1,4 +1,4 @@
 altura=20 #Altura del edificio
 a=22 #angulo que forma con los rayos del sol
-x=20/0.40 #solución
+x=20/0.40 #altura entre la tangente del angulo
 print('la longitud de la sombra es:',round (x) , 'metros')
