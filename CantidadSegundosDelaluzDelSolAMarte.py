e9fe0b6348963745c56caa00057c677fd4156a86
Omar7102 <62663061+Omar7102@users.noreply.github.com>
2020-04-16 16:25:22 -0500

    Delete CantidadSegundosDelaluzDelSolAMarte.py

--- CantidadSegundosDelaluzDelSolAMarte.py
+++
@@ -1,5 +0,0 @@
-#Programa que muestra la cantidad de segundos que le toma a la luz viajar del sol a marte.
-v=300000 # velocidad de la luz en kilometros por segundo.
-d=227940000 #Distancia entre el sol y marte en kilometros.
-t= d/v  # formula del tiempo
-print(round(t),'segundos toma la luz en viajar del sol a marte') 
